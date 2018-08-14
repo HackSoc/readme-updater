9530c6704f26cd93f6d91fac9af238cd65e4002a
Luke Moll <luke.moll99@gmail.com>
2018-08-14 20:19:07 +0100

    Initial commit (beta.hacksoc.org)

---
+++ hook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from bottle import post, run
+import platform, subprocess, os
+
+def cmd(cmd, stdout=subprocess.DEVNULL):
+    return subprocess.Popen(cmd, shell=True, env=env, cwd=cwd, stdout=stdout)
+
+url = "http://beta.hacksoc.org/servers/"
+hostname = platform.node().split(".")[0]
+outfile = "{}.html".format(hostname) # TODO override this with argv
+
+@post('/update')
+def update():
+    cmd('wget "{0}{1}.html" -O {2}'.format(url,hostname,outfile), stdout=subprocess.STDOUT).wait()
+
+run(port=7000)
