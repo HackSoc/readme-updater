91d88a0bcd8b6d6a0a65d1190c2c0a47560cb09a
Luke Moll <luke.moll99@gmail.com>
2018-08-17 18:19:41 +0100

    Added cwd

--- hook.py
+++ hook.py
@@ -4,10 +4,10 @@ from bottle import post, run
 import platform, subprocess, os
 
 env = os.environ
-
+cwd = os.path.dirname(os.path.realpath(__file__))
 
 def cmd(cmd, stdout=subprocess.DEVNULL):
-    return subprocess.Popen(cmd, shell=True, env=env, stdout=stdout)
+    return subprocess.Popen(cmd, shell=True, env=env, cwd=cwd stdout=stdout)
 
 url = "https://www.hacksoc.org/servers/"
 hostname = platform.node().split(".")[0]
